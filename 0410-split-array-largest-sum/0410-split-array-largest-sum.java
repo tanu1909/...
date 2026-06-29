c2c6543f719fb8386052b26984c6fe631af42881
Tanu Choudhary <tanuchoudhary1319@gmail.com>
2026-06-30 00:08:47 +0530

    Time: 0 ms (100%), Space: 43.1 MB (51.75%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,45 @@
+class Solution {
+
+
+    private boolean possible(int[] nums, long maxSum, long maxPartitions){
+        long partitions = 1;
+        long currSum=0;
+
+        for(int num:nums){
+            if(num>maxSum)return false;
+            if(num+currSum <= maxSum){ //if does not exceed max poss.sum, then we can add
+                currSum+=num;
+            }else{ //if exceeds maxSum, start new partition
+                partitions++;
+                currSum=num;
+                if(partitions> maxPartitions)return false;
+            }
+        }
+         return true;
+    }
+
+
+
+    public int splitArray(int[] nums, int k) {
+        long totalSum=0;
+        long largest=nums[0];
+
+        for(int num:nums){
+            totalSum+=num;
+            largest = Math.max(largest, num);
+        }
+
+        //  Max Element <= ans <=totalSum....upper nd lower bound for Bin search
+
+       long low = largest;
+       long high = totalSum;
+       
+       while(low<=high){
+        long mid = low + (high- low)/2;
+
+        if(possible(nums, mid, k)) high = mid-1; //try smaller partition
+        else low = mid+1; //if not possible try larger sum partition
+       }
+       return (int)low;
+    }
+}
